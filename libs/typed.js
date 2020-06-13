0eb89e056e2a4e86fef55ff91aa33fc9038af4e1
earowang <earo.wang@gmail.com>
2020-06-13 21:57:34 +1200

    almost done

---
+++ libs/typed.js
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    '<pre>data["instructor"]</pre>', '<pre>data["researcher"]</pre>', '<pre>data["visualiser"]</pre>'
+  ],
+  typeSpeed: 60,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
